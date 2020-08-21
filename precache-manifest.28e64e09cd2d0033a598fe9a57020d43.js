3bb88d06271c431796a78121638addbbdf4baf7b
pson0007 <ping.song@monash.edu>
2020-08-21 13:34:14 +1000

    Updates

---
+++ precache-manifest.28e64e09cd2d0033a598fe9a57020d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479e3238e56ad1159cbee8b1413d16c8",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "7ba8dbd68296711de266",
+    "url": "/mywebsite/static/css/main.96396082.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/mywebsite/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mywebsite/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba8dbd68296711de266",
+    "url": "/mywebsite/static/js/main.5e2360b2.chunk.js"
+  },
+  {
+    "revision": "27cdc9c18f62965d4573",
+    "url": "/mywebsite/static/js/runtime-main.d6e467e2.js"
+  }
+]);
